c636def9201dfe00aff586a9b094a40b6f461b2d
cableyesto <dialkins@tutamail.com>
2026-03-30 16:53:34 +0200

    feat: Initialize the main App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,46 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faKey, faUser } from '@fortawesome/free-solid-svg-icons'
-import { useState } from 'react'
-import Input from './components/general/Input'
-import SelectInput from './components/general/SelectInput'
-import Button from './components/general/Button'
-import SearchBar from './components/general/SearchBar'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Dashboard from "./routes/dashboard/Dashboard";
+import Authentication from "./routes/auth/Authentication";
 
 function App() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userUsername, setUserUsername] = useState("");
 
-  return (
-    <>
-      <div className="App">
-        <p>Cinema</p>
-        <Input
-          label="Username"
-          type="text"
-          value={email}
-          setValue={setEmail}
-          icon={<FontAwesomeIcon icon={faUser} />}
-        />
-        <Input
-          label="Password"
-          type="password"
-          value={password}
-          setValue={setPassword}
-          icon={<FontAwesomeIcon icon={faKey} />}
-        />
-        <Input
-          label="Min Date"
-          type="number"
-          value={null}
-          setValue={null}
-        />
-        <SelectInput
-          label="Sort"
-          options={["Default", "Latest", "Oldest", "Highest Rated", "Lowest Rated"]}
-        />
-        <p>Test</p>
-        <div>
-          <Button
-            label="Load more..."
-          />
-          <p>Separator</p>
-          <Button
-            label="Sign Up"
-            icon={<FontAwesomeIcon icon={faKey} />}
-          />
-        </div>
-        <div><p>Separator</p></div>
-        <SearchBar /> 
-      </div>
-    </>
-  )
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+
+    if (!accessToken) return;
+
+    const authenticateUser = async () => {
+      try {
+        const response = await axios.post(
+          "/api/auth/",
+          {}, // Empty body for POST
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
+
+        setIsLoggedIn(true);
+        setUserUsername(response.data.username || "");
+      } catch (error) {
+        setIsLoggedIn(false);
+        setUserUsername("");
+      }
+    }; 
+
+    authenticateUser();
+  }, []);
+
+  return isLoggedIn ? (
+    <Dashboard username={userUsername} />
+  ) : (
+    <Authentication />
+  );
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+// import AppTest from './AppTest.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    {/* <AppTest /> */}
   </StrictMode>,
 )
